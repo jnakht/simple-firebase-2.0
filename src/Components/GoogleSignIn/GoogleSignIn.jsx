f4e98ef4d46429ccf26f4f39cab2609cb7258978
jnakht <jisan.cse.cu@gmail.com>
2025-03-25 13:03:13 +0600

    user data on state

--- src/Components/GoogleSignIn/GoogleSignIn.jsx
+++ src/Components/GoogleSignIn/GoogleSignIn.jsx
@@ -1,16 +1,19 @@
 import { getAuth, signInWithPopup } from "firebase/auth";
 import app from "../Firebase/firebase.init";
 import { GoogleAuthProvider } from "firebase/auth";
+import { useState } from "react";
 
 
 const GoogleSignIn = () => {
+    const [user, setUser] = useState(null);
     const auth = getAuth(app);
     const googleProvider = new GoogleAuthProvider();
     const handleGoogleSignIn = () => {
         signInWithPopup(auth, googleProvider)
         .then(result => {
-            const user = result.user;
-            console.log(user);
+            const signedInUser = result.user;
+            console.log(signedInUser);
+            setUser(signedInUser);
         })
         .catch(error => {
             console.log("google error: ", error.message);
